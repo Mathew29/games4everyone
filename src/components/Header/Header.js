c629576abd43c769fca35fecebed4f9700da3ef5
Mathew <mwakre29@gmail.com>
2021-01-28 17:38:12 -0800

    styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ import styles from "./Header.module.css"
 const Header = () => {
   return (
     <div className={styles.container}>
-      <div>logo</div>
       <div>search bar</div>
       <div>signin / cart</div>
     </div>
