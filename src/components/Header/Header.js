4627dacc6f2c04ea6ff134d292bc63e2db4586ee
Mathew <mwakre29@gmail.com>
2021-01-28 18:29:14 -0800

    import search input

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import SearchInput from "../SearchInput/SearchInput";
 
 import styles from "./Header.module.css"
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      <div>search bar</div>
+      <SearchInput placeholder="Search for games" />
       <div>signin / cart</div>
     </div>
   );
