a83c9d0aeb0553baeb597d9131554449f4eda721
Mathew <mwakre29@gmail.com>
2021-01-28 16:51:05 -0800

    change title

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,14 +6,20 @@ const Layout = ({children}) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Games4Everyone</title>
       </Head>
       <header className={styles.header}>header</header>
 
       
       <nav className={styles.nav}>nav</nav>
+
+
       <main className={styles.main}>{children}</main>
+
+      
       <aside className={styles.aside}>aside</aside>
+
+
       <footer className={styles.footer}>footer</footer>
     </div>
   );
